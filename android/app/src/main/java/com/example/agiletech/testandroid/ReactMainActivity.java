3a4eb72856d3921f7ea1ab472b6ccbbd9a935120
Thang Nguyen <thangnv1509@gmail.com>
2019-01-24 09:54:48 +0700

    first integrate

---
+++ android/app/src/main/java/com/example/agiletech/testandroid/ReactMainActivity.java
@@ -0,0 +1,11 @@
+package com.example.agiletech.testandroid;
+
+import com.facebook.react.ReactActivity;
+
+public class ReactMainActivity extends ReactActivity {
+
+    @Override
+    protected String getMainComponentName() {
+        return "testReactNative";
+    }
+}
